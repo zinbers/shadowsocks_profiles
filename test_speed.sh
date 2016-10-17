782ac4ecae839b3b756faeb99d64bb28fa0188a5
张冰 <359565935@qq.com>
2016-10-17 20:38:46 +0800

    modify testspeed shell

--- test_speed.sh
+++ test_speed.sh
@@ -4,12 +4,13 @@ if [ $1 ]; then
     server_file=$1
 fi
 server_list=(`cat $server_file| grep \"server\" | cut -d ":" -f 2`)
-min_time=5000
-min_server=""
+#min_time=5000
+#min_server=""
 total_server=${#server_list[@]}
+ret_str=""
 for((i=1;i<=$total_server;i++))
 do
-    echo "--process $i/$total_server"
+    echo "->process $i/$total_server"
     serv=`echo ${server_list[i-1]} | tr -d '"'`
     ret=`ping -t 5 $serv`
     time=(`echo $ret | tr ' ' '\n' | grep "time"`)
@@ -29,13 +30,18 @@ do
     if [ $flag -gt 0 ]; then
         sum_time=`echo "scale=3;$sum_time/$counter"|bc`
         echo "server:$serv avg_time:$sum_time"
-        flag=`echo "$min_time > $sum_time" |bc`
-        if [ $flag -gt 0 ]; then
-            min_time=$sum_time
-            min_server=$serv
-        fi
+        #flag=`echo "$min_time > $sum_time" |bc`
+        #if [ $flag -gt 0 ]; then
+            #min_time=$sum_time
+            #min_server=$serv
+        #fi
     else
         echo "server:$serv avg_time:timeout"
+        sum_time=5000
     fi
+    ret_str="$ret_str\n$serv $sum_time" 
+    echo
 done
-echo "fin min_server:$min_server min_time:$min_time"
+#echo "fin min_server:$min_server min_time:$min_time"
+echo "->Top 10"
+echo "$ret_str" | sort -n -k 2 | head -10
