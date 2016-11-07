9b31283d5ac7d83242b42e05bee35c4b9c6b10cf
张冰 <359565935@qq.com>
2016-11-07 10:00:51 +0800

    + auto restart shadowsocks

--- add.sh
+++ add.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+function killShadowsocksX(){
+    ss_process=`ps aux | grep "ShadowsocksX" | grep -v "grep"`
+    if [ "$ss_process" ]; then
+        pid=`echo "$ss_process" | awk '{print $2}'`
+        if [ -z "$pid" ]; then
+            echo "kill shadowsocks process failed, please restart ShadowsocksX manually!"
+        else
+            kill -9 "$pid"
+        fi
+    fi
+}
+
+function restartShadowsocksX(){
+    ss_app_path="/Applications/ShadowsocksX.app"
+    if [ -d "$ss_app_path" ]; then
+        open "$ss_app_path"
+        echo "restart ShadowSocksX success."
+    fi
+}
+
+ss_cfg=${1:-"ss_cfg.json"}
 if [ -s "$HOME/Library/Preferences/clowwindy.ShadowsocksX.plist" ]; then
     plutil -convert xml1 $HOME/Library/Preferences/clowwindy.ShadowsocksX.plist -o tmp.xml
     lines=(`grep -n "data" tmp.xml | cut -d ':' -f 1`)
@@ -6,8 +27,8 @@ if [ -s "$HOME/Library/Preferences/clowwindy.ShadowsocksX.plist" ]; then
     data_end=${lines[1]}
     data_start_next=$[$data_start + 1]
     data_end_last=$[$data_end - 1]
-    if [ -s ./ss_cfg.json ]; then
-        encodeJson=`cat ./ss_cfg.json | base64`
+    if [ -s "$ss_cfg" ]; then
+        encodeJson=`cat "$ss_cfg" | base64`
         sed -i.bak "${data_start_next},${data_end_last}d;${data_start}a\\$encodeJson" ./tmp.xml
     else
         echo "can not find json config file or it's empty!"
@@ -16,11 +37,14 @@ if [ -s "$HOME/Library/Preferences/clowwindy.ShadowsocksX.plist" ]; then
     plutil -convert binary1 ./tmp.xml -o ./tmp.plist
     defaults import clowwindy.ShadowsocksX ./tmp.plist
     rm tmp.xml tmp.xml.bak tmp.plist
-    echo "done! please restart shadowsocks!"
+    echo "done!"
+    killShadowsocksX
+    restartShadowsocksX
 else
     echo "can not find shadowsocks.plist or it's empty!"
     exit 1
 fi
+
 
 
 
